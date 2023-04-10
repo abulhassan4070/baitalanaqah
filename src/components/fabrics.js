9e1f5b6d2985feacc9b9abb9a74f4ae181a3f84a
Gandhi Kumar <gandhikumar86@gmail.com>
2023-04-10 15:52:30 +0530

    Removed unused variable

--- src/components/fabrics.js
+++ src/components/fabrics.js
@@ -13,15 +13,6 @@ import React from "react";
 import { useState } from "react";
 import "assets/css/components.css";
 
-const breakpoints = {
-  base: "0em", // 0px
-  sm: "30em", // 480px
-  md: "48em", // 768px
-  lg: "62em", // 992px
-  xl: "80em", // 1280px
-  "2xl": "96em", // 1536px
-};
-
 const FabricList = (props) => {
   return (
     <VStack align={"left"}>
